6103ba28a7bfaa0459dcdfab92ad279757f78539
Goga <gogaimerlishvili1@gmail.com>
2022-08-01 07:35:34 +0400

    added path pages of navbar in Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         {/* <Route exact path="/" element={<Navbar />} /> */}
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
       </Routes>
     </div>
   );
