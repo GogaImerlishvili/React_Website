4d5bf9cf5493e31e6c532f5c48fd168a3bda6a4b
Goga <gogaimerlishvili1@gmail.com>
2022-08-01 04:28:02 +0400

    added homepage

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../index.css";
+import HeroSection from "../HeroSection";
+import Navbar from "../Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default Home;
