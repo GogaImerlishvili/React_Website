b1d4a4a52e1edf01f3c2884399af95e3437c85b4
Goga <gogaimerlishvili1@gmail.com>
2022-08-01 07:36:19 +0400

    added new imgs for this section

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,10 @@ import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
 import adventure from "../imgs/img-9.jpg";
+import bali from "../imgs/img-2.jpg";
+import img3 from "../imgs/img-3.jpg";
+import img4 from "../imgs/img-4.jpg";
+import img8 from "../imgs/img-8.jpg";
 const Cards = () => {
   return (
     <div className="cards">
@@ -17,6 +21,34 @@ const Cards = () => {
                 path="/services"
               />
             }
+            <CardItem
+              src={bali}
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            {
+              <CardItem
+                src={img3}
+                text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                label="Adventure"
+                path="/services"
+              />
+            }
+            <CardItem
+              src={img4}
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Luxury"
+              path="/services"
+            />
+            <CardItem
+              src={img8}
+              text="Ride through the Sahara Desert on a guided"
+              label="Luxury"
+              path="/services"
+            />
           </ul>
         </div>
       </div>
