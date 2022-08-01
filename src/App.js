a19e7ea7cbcd21c42641116e6f272f719e2df21e
Goga <gogaimerlishvili1@gmail.com>
2022-08-01 04:27:48 +0400

    added homepage in route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="container">
       <Routes>
-        <Route exact path="/" element={<Navbar />} />
+        {/* <Route exact path="/" element={<Navbar />} /> */}
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </div>
   );
